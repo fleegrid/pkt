2cec4b1dd75e705a2124a69bee1f0ca9a8c31ecc
YoRHa No.2 Type B <unit.2b@yorha.army>
2017-07-18 14:37:24 +0800

    add SetProto CopyPayload to TUNPacket

--- tun.go
+++ tun.go
@@ -19,6 +19,24 @@ func (p TUNPacket) Proto() (uint16, error) {
 	return uint16(p[2])<<8 + uint16(p[3]), nil
 }
 
+// SetProto set the proto of TUN device
+func (p TUNPacket) SetProto(proto uint16) error {
+	if len(p) < 4 {
+		return ErrTooShort
+	}
+	p[2] = byte(proto >> 8)
+	p[3] = byte(proto)
+	return nil
+}
+
+// CopyPayload copy bytes to payload
+func (p TUNPacket) CopyPayload(b []byte) (int, error) {
+	if len(p) < 4 {
+		return 0, ErrTooShort
+	}
+	return copy(p[4:], b), nil
+}
+
 // Payload get the payload of TUNPacket
 func (p TUNPacket) Payload() ([]byte, error) {
 	if len(p) < 4 {
--- tun_test.go
+++ tun_test.go
@@ -1,1 +1,30 @@
 package pkt
+
+import (
+	"bytes"
+	"syscall"
+	"testing"
+)
+
+func TestTUNPacket(t *testing.T) {
+	p := make(TUNPacket, 10)
+	p.SetProto(syscall.AF_INET)
+	if p[3] != syscall.AF_INET {
+		t.Errorf("failed to set proto")
+	}
+	b := []byte{1, 2, 3, 4, 5, 6}
+	l, err := p.CopyPayload(b)
+	if err != nil {
+		t.Errorf("failed to set payload")
+	}
+	if l != 6 {
+		t.Errorf("set payload returns wrong count")
+	}
+	nb, _ := p.Payload()
+	if !bytes.Equal(b, nb) {
+		t.Errorf("not equal")
+	}
+	if !bytes.Equal(p, []byte{0, 0, 0, syscall.AF_INET, 1, 2, 3, 4, 5, 6}) {
+		t.Errorf("not equal")
+	}
+}
